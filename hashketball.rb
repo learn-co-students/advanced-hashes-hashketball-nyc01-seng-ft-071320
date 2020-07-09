36c192824a8819288fb299537249119a25221837
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-09 15:13:56 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -126,4 +126,80 @@ def game_hash
   }
 end
 
-# Write code here
+ def num_points_scored(player_search)
+   game_hash.each do |team, team_data|
+     team_data[:players].each do |player|
+       if player[:player_name] == player_search
+         return player[:points]
+       end
+     end
+   end
+ end # Write code here
+
+def shoe_size(name)
+  game_hash.each do |team, team_data|
+  team_data[:players].each do |player|
+    if player[:player_name] == name
+      return player[:shoe]
+    end
+  end
+end
+end
+
+def team_colors(team_input)
+  if team_input.downcase == "charlotte hornets"
+    return game_hash[:away][:colors]
+  else
+    return game_hash[:home][:colors]
+  end
+end
+
+def team_names
+  game_hash.map do |team, team_data|
+    team_data[:team_name]
+  end
+end
+
+def player_numbers(data)
+  output = []
+  game_hash.each do |team, team_data|
+    if team_data[:team_name] == data
+      team_data.each do |key, value|
+        if key == :players
+          value.each do |player|
+            output.push(player[:number])
+          end
+        end
+      end
+    end
+  end
+  return output
+end
+
+def player_stats(data)
+  game_hash.each do |team, team_data|
+    team_data.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if data == player[:player_name]
+            return player
+          end
+        end
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+  big_shoe = 0
+  rebounds = 0
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  return rebounds
+end
