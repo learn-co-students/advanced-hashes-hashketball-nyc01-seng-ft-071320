99fccc97475f4c785a071ceedd9598a32ae4b680
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-11 03:49:30 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require './hashketball.rb'
 # Write your code below game_hash
 def game_hash
   {
@@ -126,4 +127,72 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored (player_name)
+  game_hash.values.each do |team|  
+    team[:players].each do |player|
+      return player[:points] if player.has_value?(player_name)
+    end
+  end
+end
+
+
+def shoe_size(player_name)
+
+  game_hash.values.each do |team_info|  
+    team_info[:players].each do |player|
+      return player[:shoe] if player.has_value?(player_name)
+    end
+  end
+
+end
+
+def team_colors(team)
+
+  game_hash.values.each do |team_info|
+    if team_info.has_value?(team)
+      return team_info[:colors].map(&:capitalize)
+    end
+  end
+end
+
+def team_names
+  game_hash.values.map {|value| value[:team_name]}
+
+end
+
+def player_numbers(team)
+
+  game_hash.values.each do |team_info|
+    if team_info.has_value?(team)
+      return team_info[:players].map { |player| player[:number]}
+    end
+  end
+end
+
+
+def player_stats(player_name)
+
+  game_hash.values.each do |team_info|
+    team_info[:players].each do |player|
+      if player.has_value?(player_name)
+         player.delete (player_name) 
+         return player
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+
+  biggest_shoe = 0
+  player_rebounds = 0
+  game_hash.values.each do |team_info|
+    team_info[:players].each do |player|
+      if player[:shoe] > biggest_shoe
+        biggest_shoe = player[:shoe] 
+        player_rebounds = player[:rebounds] 
+      end
+    end
+  end
+  return player_rebounds
+end
